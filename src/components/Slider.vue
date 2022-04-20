b2521a30a0d6d069ada5b60ade20103d1bb1e777
Alan Nabiev <alannabiev2001@gmail.com>
2022-04-20 23:09:19 +0300

    fix: styles

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -22,12 +22,12 @@ const sliderClasses = computed(() => {
       :class="`flex mobile:justify-between mobile:relative  mobile:w-[320%] transition-transform duration-500 ${sliderClasses}`"
     >
       <div class="w-full relative flex justify-center">
-        <div class="absolute -left-6 mobile:left-8 bottom-10 flex flex-col">
+        <div class="absolute left-12 mobile:left-8 bottom-10 flex flex-col">
           <div class="flex space-x-[5px] items-center">
             <BaseDiagram />
             <p class="italic">white / dry</p>
           </div>
-          <div class="my-2 h-px bg-[#333333] w-56 mobile:w-32"></div>
+          <div class="my-2 h-px bg-[#333333] w-36 mobile:w-32"></div>
           <p class="font-grotesk font-medium text-2xl mobile:text-lg">Riesling</p>
         </div>
         <Bottle name="blue" class="w-full" />
@@ -45,13 +45,13 @@ const sliderClasses = computed(() => {
       </div>
       <div class="w-full relative flex justify-center">
         <div
-          class="absolute -right-6 mobile:right-0 top-10 flex flex-col items-end"
+          class="absolute right-2 mobile:right-0 top-10 flex flex-col items-end"
         >
           <div class="flex space-x-[5px] items-center">
             <BaseDiagram color="red" :currentValue="59" />
             <p class="italic">white / semi-sweet</p>
           </div>
-          <div class="my-2 h-px bg-[#333333] w-56 mobile:w-48"></div>
+          <div class="my-2 h-px bg-[#333333] w-48 mobile:w-48"></div>
           <p class="font-grotesk font-medium text-2xl mobile:text-lg">Chardonnay</p>
         </div>
         <Bottle name="red" class="w-full" />
